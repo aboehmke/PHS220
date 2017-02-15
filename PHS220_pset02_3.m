ebbb5dc5f09bc3fc458a8d2122104ad1c8aa6703
aboehmke <aboehmke@wellesley.edu>
2017-02-14 20:15:33 -0500

    Add files via upload

---
+++ PHS220_pset02_3.m
@@ -0,0 +1,33 @@
+% Alex Boehmke
+% pset02, due 08Feb
+clear all
+
+r = 1;
+v = 1; 
+dtheta = 0.1; % select increment
+theta = 0:dtheta:(10*pi);
+x = [ ];
+y = [ ];
+% radiusx = [ ];
+% radiusy = [ ];
+for t = 1:150
+    % (a) circle
+    circlex = r.*cos(theta) + theta(t);
+    circley = r.*sin(theta) + 1;
+    % (b) cycloid
+    x(t) = r*(theta(t) - sin(theta(t))); % circle centered at (r*theta,r)
+    y(t) = r*(1 - cos(theta(t)));
+    % (c) radius
+    
+   
+    plot(x,y,'r-',circlex,circley,'k-')
+    hold on
+    axis([-2,20,-4,4]);
+    ax = gca;
+    ax.XAxisLocation = 'origin';
+    ax.YAxisLocation = 'origin';
+    axis equal
+    drawnow
+    pause(0.1)
+    hold off
+end
