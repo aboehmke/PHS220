1f21d20f6fc7a179661d67a808fe6334f8d06810
aboehmke <aboehmke@wellesley.edu>
2017-02-20 10:11:31 -0500

    added radius

--- PHS220_pset02_3.m
+++ PHS220_pset02_3.m
@@ -1,32 +1,39 @@
 % Alex Boehmke
 % pset02, due 08Feb
-% Animate a cycloid.
+% animate a cycloid
 clear all
 
-r = 1; % circle radius
-v = 1; % translational velocity 
-dTHETA = 0.1; % select rotation increment
-THETA = 0:dTHETA:(10*pi);
-X = [ ];
-Y = [ ];
-% radiusx = [ ];
-% radiusy = [ ];
+r = 2;
+v = 1; 
+dtheta = 0.1; % select increment
+theta = 0:dtheta:(10*pi); % or time
+x = [ ];
+y = [ ];
 for t = 1:150
     % (a) circle
-    circleX = r.*cos(THETA) + THETA(t);
-    circleY = r.*sin(THETA) + 1;
+    circlex = r.*cos(theta) + r*theta(t);
+    circley = r.*sin(theta) + r;
     % (b) cycloid
-    X(t) = r*(THETA(t) - sin(THETA(t))); % circle centered at (r*theta,r)
-    Y(t) = r*(1 - cos(THETA(t)));
+    x(t) = r*(theta(t) - sin(theta(t))); % circle centered at (r*theta,r)
+    y(t) = r*(1 - cos(theta(t)));
+    % (c) radius
+    % the two points of the radius line are
+    % center: (theta,1) and  edge:(x,y)
+    radiusx = [(r * theta(t)),x(t)];
+    radiusy = [r,y(t)];
    
-    plot(X,Y,'r-',circleX,circleY,'k-')
-    hold on
-    axis([-2,20,-4,4]);
+    plot(x,y,'r-',circlex,circley,'k-',radiusx,radiusy,'g-')
+    xlabel('x');
+    ylabel('y');
+    title('cycloid animation')
+    %axis([-r,4*2*pi*r,-2,3*r]);
+    axis equal
     ax = gca;
     ax.XAxisLocation = 'origin';
     ax.YAxisLocation = 'origin';
-    axis equal
+    hold on
     drawnow
-    pause(0.1)
-    hold off
+    pause(0.1) % btwn each iteration of the loop, so btw each re-drawing of circle
+    hold off % hold off at the end of each iteration to erase circle and radius
 end
+
